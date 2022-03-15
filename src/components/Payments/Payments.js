9de9796dc388e63f0c8e0401d5d9ea56fb04d73e
WalukaUI <cwaluka@yahoo.com>
2022-03-15 14:27:16 -0700

    payment page styling

--- src/components/Payments/Payments.js
+++ src/components/Payments/Payments.js
@@ -1,12 +1,37 @@
 import "./_payments.scss";
+import PaymentCare from "./payment-care.jpg";
 
 export default function Payments() {
   return (
     <>
-      <div className="payments row">
-        <div className="col col-md-6 col-sm-12 col-xs-12 paymentsImage"></div>
-        <div className="col col-md-6 col-sm-12 col-xs-12">
-          <h5>Pyaments page</h5>
+      <div className="payments paymentsImage">
+        <div className="row">
+          <div className="col col-md-6 col-sm-12 col-xs-12 mx-auto">
+            <img src={PaymentCare} alt="care" />
+            <p style={{ fontSize: "7px" }}>
+              Photo by{" "}
+              <a href="https://www.pexels.com/photo/man-in-white-shirt-and-man-in-black-shirt-7551686/">
+                Kampus Production
+              </a>{" "}
+              from Pexels
+            </p>
+          </div>
+          <div className="col col-md-6 col-sm-12 col-xs-12 mx-auto">
+            Now you can have our service as low as $30 per session.
+            <br />
+            <br />
+            <h6>We accept CashApp and Zelle as payment methords.</h6>
+          </div>
+        </div>
+        <div className="attribute">
+          Photo by{" "}
+          <a href="https://unsplash.com/@blankerwahnsinn?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Fabian Blank
+          </a>{" "}
+          on{" "}
+          <a href="https://unsplash.com/collections/9544423/banking?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Unsplash
+          </a>
         </div>
       </div>
     </>
