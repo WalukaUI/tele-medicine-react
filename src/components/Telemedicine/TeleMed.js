1f1300da10017873ab2e7006743ad110bf5e33d8
WalukaUI <cwaluka@yahoo.com>
2022-03-16 19:58:59 -0500

    telemed page edited

--- src/components/Telemedicine/TeleMed.js
+++ src/components/Telemedicine/TeleMed.js
@@ -1,16 +1,30 @@
 import "./_telemed.scss";
+import Telemed from "./telemed.jpg";
 
 export default function TeleMed() {
   return (
     <>
-      <div className="teleMed row">
-        <div className="col col-md-6 col-sm-12 col-xs-12"></div>
-        <div className="col col-md-6 col-sm-12 col-xs-12">
-          <h5>
-            When you are sick and don't want to leave home we are here to offer
-            affordable and convenient phone and video consultation to take care
-            of your medical needs !
-          </h5>
+      <div className="telemedImage">
+        <div className="row telemed">
+          <div className="col col-md-6 col-sm-12 mx-auto">
+            <img src={Telemed} alt="telemedicine" />
+            <p style={{ fontSize: "7px" }}>Photo by </p>
+          </div>
+          <div className="col col-md-6 col-sm-12mx-auto">
+            Now you can have our service as low as $25 onwards per session.
+            <br />
+            <br />
+            <h6>
+              We accept CashApp, Zelle and Debit/ credit card payment methords.
+            </h6>
+          </div>
+        </div>
+        <div className="attribute">
+          Photo by{" "}
+          <a href="https://www.pexels.com/photo/three-beige-yellow-and-pink-heart-marshmallows-612825/">
+            Ylanite Koppens
+          </a>{" "}
+          from Pexels
         </div>
       </div>
     </>
