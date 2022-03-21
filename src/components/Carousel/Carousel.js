4bcc51da6f45047c8949fcd937b74b19d0438ac6
WalukaUI <cwaluka@yahoo.com>
2022-03-20 19:03:30 -0500

    mobile responsiveness edited appointmnet booking page

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,4 +1,4 @@
-import { React, useEffect } from "react";
+import { React } from "react";
 import { Carousel } from "react-bootstrap";
 import "./_carousel.scss";
 import Dark from "./images/dark.jpg";
